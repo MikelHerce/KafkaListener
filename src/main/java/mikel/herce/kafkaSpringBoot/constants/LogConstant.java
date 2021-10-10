f218fed21ddde29d076fc231f30bf0dad76c7db7
Mikel <mikelherce87@gmail.com>
2021-10-10 13:49:47 +0200

    logger

---
+++ src/main/java/mikel/herce/kafkaSpringBoot/constants/LogConstant.java
@@ -0,0 +1,8 @@
+package mikel.herce.kafkaSpringBoot.constants;
+
+public final class LogConstant {
+
+	public static final String DOUBLE_POINT_SPACE = ": ";
+	public static final String MESSAGE_ADDED = "Message added";
+	public static final String MESSAGE_DELETED = "All message deleted";
+}
--- src/main/java/mikel/herce/kafkaSpringBoot/messages/service/MessageServiceImpl.java
+++ src/main/java/mikel/herce/kafkaSpringBoot/messages/service/MessageServiceImpl.java
@@ -3,19 +3,25 @@ package mikel.herce.kafkaSpringBoot.messages.service;
 import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mikel.herce.kafkaSpringBoot.constants.LogConstant;
 import mikel.herce.kafkaSpringBoot.messages.repository.MessageRespository;
 
 @Service
 public class MessageServiceImpl implements MessageService {
+	
+	private static Logger LOG = LoggerFactory.getLogger(MessageServiceImpl.class);
 	
 	@Autowired
 	MessageRespository messageRepository;
 
 	@Override
 	public void addMessage(String message) throws IOException {
+		LOG.info(LogConstant.MESSAGE_ADDED + LogConstant.DOUBLE_POINT_SPACE + message);
 		messageRepository.addMessageToBuffer(message);
 	}
 
@@ -26,6 +32,7 @@ public class MessageServiceImpl implements MessageService {
 
 	@Override
 	public void deleteAllMessages() {
+		LOG.info(LogConstant.MESSAGE_DELETED);
 		messageRepository.deleteMessages();
 	}
 
